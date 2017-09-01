fe785235c930099fbe0d111060bbef8276ba7b0a
naifeng <naifengliu.us@gmail.com>
2017-09-01 13:40:04 -0400

    fix

--- eva.py
+++ eva.py
@@ -1,15 +1,26 @@
 import numpy as np
 import os
 
-v = np.zeros([39597, 2])
-t = np.zeros([49801, 2])
+# v = np.zeros([39597, 2])
+# t = np.zeros([49801, 2])
+#
+#
+# for files in os.listdir("/network/rit/lab/ceashpc/nl544752"):
+#     if files[0] == "v":
+#         v += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
+#     if files[0] == "t":
+#         t += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
+#
+# np.savetxt("v_result", v)
+# np.savetxt("t_result", t)
 
+v_result = np.loadtxt("v_result")
+t_result = np.loadtxt("t_result")
 
-for files in os.listdir("/network/rit/lab/ceashpc/nl544752"):
-    if files[0] == "v":
-        v += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
-    if files[0] == "t":
-        t += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
+v_rank = np.argsort(-v_result, axis=0)
+v_rank_final = v_rank[:, 0]
 
-np.savetxt("v_result", v)
-np.savetxt("t_result", t)
+t_rank = np.argsort(-t_result, axis=0)
+t_rank_final = t_rank[:, 0]
+
+print v_rank_final
---
+++ sim.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+test = np.array([[4, 2], [2, 1], [3, 0]])
+
+a = np.argsort(test, axis=0)
+
+print a[:,0]
