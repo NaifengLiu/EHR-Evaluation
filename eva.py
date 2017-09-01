ea9785f671fa32d4c258665cc07c81281243a79d
naifeng <naifengliu.us@gmail.com>
2017-09-01 11:57:59 -0400

    init

---
+++ eva.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+
+v = np.zeros([49801, 2])
+t = np.zeros([49801, 2])
+
+
+for files in os.listdir("/network/rit/lab/ceashpc/nl544752"):
+    if files[0] == "v":
+        v += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
+    if files[0] == "t":
+        t += np.loadtxt("/network/rit/lab/ceashpc/nl544752/" + files)
+
+np.savetxt("v_result", v)
+np.savetxt("t_result", t)
