742131fc0a5c776271f532793f2d2dc550ec5a26
Godot <naifengliu.us@gmail.com>
2017-09-26 09:45:19 -0400

    fi

--- sim.py
+++ sim.py
@@ -1,1 +1,19 @@
-print int(1.5/0.9)
+import numpy as np
+
+test = np.loadtxt("v0")[:, 0]
+
+v_rank = np.argsort(test, axis=0)
+print v_rank
+
+v_found = 0
+count = 0
+result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in v_rank:
+    if i <= 197:
+        v_found += 1
+    count += 1
+    ratio = v_found / float(197)
+    if 1 <= int(ratio/0.05) <= 10:
+        if result[int(ratio/0.05)-1] == 0:
+            result[int(ratio/0.05)-1] = v_found / float(count) * 100
+print result
